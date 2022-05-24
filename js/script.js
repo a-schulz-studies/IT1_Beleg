cc0b54891a2c143dc40adcc9fadbb28d89d9702c
Alexander Schulz <s82105@htw-dresden.de>
2022-05-24 15:54:00 +0200

    Promise eingefügt funktioniert aber nicht

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-window.addEventListener("load", start, false);
-var xhr = getXhr();
-
 // TODO:
 /* durch Abfrage prüfen, welche Themen es gibt. (Anzeige auf den entsprechenden Buttons)
 Diese sollten dann angezeigt werden. (Indizes zu den jeweiligen Aufgaben in seperatem Array abspeichern damit darauf dann später die Aufgaben angezeigt werden können.)
  */
 
+window.addEventListener("load", start, false);
+var xhr = getXhr();
 const url = "https://irene.informatik.htw-dresden.de:8888/api/quizzes/";
 
 function getXhr() {
@@ -16,65 +15,72 @@ function getXhr() {
   } else return false;
 }
 
-// soweit abgeschlossen TODO Kommentare entfernen!
 function sendXhr(method, urlExt, data = null) {
   /*
    *  method: GET/POST
    *  url_ext: Suffix der Quell-URL
    *  leer für alle Quizzes
-   *  id für spezielles quiz
-   *  id/solve zum lösen eines Quizzes
+   *  "id" für spezielles quiz
+   *  "id"/solve zum lösen eines Quizzes
    */
   let urlDest = url + urlExt;
   xhr.onreadystatechange = xhrHandler;
   xhr.open(method, urlDest);
   xhr.setRequestHeader("Content-Type", "application/json");
-  // hier evtl noch variable und Funktion, dass man pwd im Klartext eingeben kann und dann convertiert
+  // nice to have: hier evtl noch variable und Funktion, dass man pwd im Klartext eingeben kann und dann convertiert
   xhr.setRequestHeader("Authorization", "Basic dGVzdEBnbWFpbC5jb206c2VjcmV0");
   xhr.send(data);
-  console.debug("Request send");
 }
+
 function xhrHandler() {
-  // console.log("im xhrHandler");
-  // console.log("Status: " + xhr.readyState);
-  // if (xhr.readyState != 4) {
-  //   return;
-  // }
-  // console.log("Status: " + xhr.readyState + " " + xhr.status);
-  if (xhr.status == 200) {
-    text = xhr.responseText;
-    console.log(text);
-    // return text;
-
-    // p = JSON.parse(text);
-    // console.log(p.note);
-    // console.log(p.note[1]);
-    // console.log(p.note[1].a);
-    // document.getElementById("ziel").innerHTML = text;
-  }
+  new Promise((resolve) => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      data = JSON.parse(xhr.responseText);
+      // console.log(data);
+      resolve(data);
+    }
+  });
 }
-
-// //https://www.w3schools.com/js/js_json_parse.asp
 
 function start() {
-  // window.alert("Es hat funktioniert");
   var main = document.getElementById("main");
-  console.log(main);
   // getchoices fehlt noch
+  /* 
+Wie ich brauche alle unterschiedlichen titel
+und dann pro titel ein 
+array title = ["Mathe", "Akorde"];
+array AufgabetoTitel [[1, 2, 3], [4,5,6]]
+// Mathe ist mit Index 0 -> ziehe die Werte aus AufgabetoTitel mit Index 0 analog die anderen
+   */
+  let choices2 = getChoices();
   var choices = ["Mathe", "Akorde"];
-  console.log(choices);
+  // console.log(choices);
   for (let i in choices) {
-    console.log(i);
+    // console.log(i);
     var el = document.createElement("button");
     el.setAttribute("name", choices[i]);
     el.innerHTML = choices[i];
-    el.addEventListener("click", chooseTopic, false);
+    el.addEventListener("click", loadTopicPage, false);
     main.appendChild(el);
   }
 }
 
-// choosetopc kann final entfallen, da das anzeigen jeweils immer das gleiche ist.
-function chooseTopic(event) {
+function getChoices() {
+  let data;
+
+  console.log("vor dem senden");
+  sendXhr("GET", "").then((result) => {
+    console.log(result);
+  });
+  console.log("nach dem senden");
+  console.log(data);
+  console.log(data.title);
+
+  // let result = Array.from(new Set(data.title));
+  return;
+}
+
+/* function chooseTopic(event) {
   var targetElement = event.target || event.srcElement;
   // console.log(targetElement.name);
   switch (targetElement.name) {
@@ -86,10 +92,11 @@ function chooseTopic(event) {
       console.log("Erfolgreich auf Akorder geklickt");
       break;
   }
-}
+} */
 
 function loadTopicPage() {
-  console.log("vor dem Ajax Aufruf");
-  sendXhr("GET", "12");
+  // console.log("vor dem Ajax Aufruf");
+  let data = sendXhr("GET", "12");
 }
+
 // Wenn eine Antwort falsch ist, dann soll der Button dementsprechend rot (richtig -> grün) aufleuchten
