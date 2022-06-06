0b5cf5750c06fedf9c331f59f6f7493843b3156b
Alexander Schulz <s82105@htw-dresden.de>
2022-06-06 21:59:54 +0200

    funktioniert bis questions

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ window.addEventListener("load", start, false);
 const url = "https://irene.informatik.htw-dresden.de:8888/api/quizzes/";
 const username = "newtest@email.com";
 const password = "secret";
+const main = document.getElementById("main");
 
 /* Start
 startet die Funktionalität
  */
 function start() {
-  const main = document.getElementById("main");
   loadStartPage(main);
 }
 
@@ -26,18 +26,40 @@ function loadStartPage(div) {
   let options = ["Offline", "Online"];
   const inner = document.createElement("div");
   inner.setAttribute("id", "options");
-  for (let i in options) {
-    let el = document.createElement("button");
-    el.setAttribute("name", options[i]);
-    el.innerHTML = options[i];
-    el.addEventListener("click", loadTopicPage, false);
-    inner.appendChild(el);
-  }
+  createButtonsFromArray(options, inner, loadTopicPage)
   div.appendChild(inner);
 }
 /* TopicPage
 Hier werden die verschiedenen Themen der gewählten Option angezeigt
 */
-function loadTopicPage() {
+function loadTopicPage(event) {
+  clearMain();
+  const targetElement = event.target || event.srcElement;
+  switch(targetElement.name){
+    case "Offline":
+      const topics = Object.keys(sourceData);
+    	createButtonsFromArray(topics, main, loadQuestions)
+      break;
+    case "Online":
+      break;
+  }
+}
 
+function loadQuestions(event){
+  console.log("loadQuestions");
+}
+
+// Function to clear the main Div
+function clearMain(){
+  document.getElementById("main").innerHTML = "";
+}
+/* Creates Buttons in specified div with EventListener on each button */
+function createButtonsFromArray(input, where, listenerFunction){
+  for (let i in input) {
+    let el = document.createElement("button");
+    el.setAttribute("name", input[i]);
+    el.innerHTML = input[i];
+    el.addEventListener("click", listenerFunction, false);
+    where.appendChild(el);
+  }
 }
