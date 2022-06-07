7c527319682021390223f306503d187490e29cc9
Alexander Schulz <s82105@htw-dresden.de>
2022-06-07 16:12:09 +0200

    Solutions werden angezeigt geordnet

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 // TODO:
-/* durch Abfrage prüfen, welche Themen es gibt. (Anzeige auf den entsprechenden Buttons)
+/* 
+-> ZurückButton
+-> HomeButton
+
+durch Abfrage prüfen, welche Themen es gibt. (Anzeige auf den entsprechenden Buttons)
 Diese sollten dann angezeigt werden. (Indizes zu den jeweiligen Aufgaben in seperatem Array abspeichern damit darauf dann später die Aufgaben angezeigt werden können.)
  */
 
@@ -10,12 +14,13 @@ window.addEventListener("load", start, false);
 const url = "https://irene.informatik.htw-dresden.de:8888/api/quizzes/";
 const username = "newtest@email.com";
 const password = "secret";
-const main = document.getElementById("main");
+var prevPage = "";
 
 /* Start
 startet die Funktionalität
- */
+*/
 function start() {
+  const main = document.getElementById("main");
   loadStartPage(main);
 }
 
@@ -23,30 +28,63 @@ function start() {
 Hier kann man zwischen dem Offline und Online Modus wählen kann.
 */
 function loadStartPage(div) {
+  // prevPage = "loadStartPage";
   let options = ["Offline", "Online"];
   const inner = document.createElement("div");
   inner.setAttribute("id", "options");
-  createButtonsFromArray(options, inner, loadTopicPage)
+  createButtonsFromArray(options, inner, loadTopicPage);
   div.appendChild(inner);
 }
 /* TopicPage
 Hier werden die verschiedenen Themen der gewählten Option angezeigt
 */
 function loadTopicPage(event) {
+  // prevPage = "loadStartPage";
   clearMain();
   const targetElement = event.target || event.srcElement;
   switch(targetElement.name){
     case "Offline":
       const topics = Object.keys(sourceData);
-    	createButtonsFromArray(topics, main, loadQuestions)
+    	createButtonsFromArray(topics, main, loadOfflineQuestions)
       break;
-    case "Online":
-      break;
+      case "Online":
+        break;
+      }
+    }
+    
+
+function loadOfflineQuestions(event){
+  // prevPage = "loadTopicPage(Offline)"; //Muss noch angepasst werden (event)
+  clearMain();
+  const targetElement = event.target || event.srcElement;
+  const key = targetElement.name
+  // console.log(sourceData[key]);
+  const data = sourceData[key];
+  for(let i in data){
+    console.log(data[i]);
+    // Aufgabenstellung erstellen
+    const quest = document.createElement("div");
+    quest.setAttribute("id", "question");
+    quest.innerHTML = data[i].a;
+    main.appendChild(quest);
+    
+    // Mögliche Lösungen erstellen
+    const solution = document.createElement("div");
+    solution.setAttribute("id", "solution");
+    createButtonsFromArray(data[i].l, solution, submitAnswer);
+    main.appendChild(solution);
+
+    waitForAnswer();
   }
 }
 
-function loadQuestions(event){
-  console.log("loadQuestions");
+function waitForAnswer(){
+  
+}
+
+function submitAnswer(){
+
+
 }
 
 // Function to clear the main Div
