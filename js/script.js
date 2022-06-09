1dc4e9fcd6e349451430c00096af950b62ed8a19
Alexander Schulz <s82105@htw-dresden.de>
2022-06-09 13:01:26 +0200

    Statistikseite funktioniert

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@
 -> ZurückButton
 -> HomeButton
 
-durch Abfrage prüfen, welche Themen es gibt. (Anzeige auf den entsprechenden Buttons)
-Diese sollten dann angezeigt werden. (Indizes zu den jeweiligen Aufgaben in seperatem Array abspeichern damit darauf dann später die Aufgaben angezeigt werden können.)
+- Darstellung der MatheAufgaben soll durch Katex erfolgen
+- Reihenfolge der Aufgaben soll auch zufällig sein
+- Statistik am Ende jedes Durchlaufs
+- Responsive Design mittels mediaquerys
  */
 
 // Initializig Functionality
@@ -16,6 +18,7 @@ const username = "newtest@email.com";
 const password = "secret";
 var prevPage = "";
 let topic;
+let statsOffline = {};
 
 /* Start
 startet die Funktionalität
@@ -54,15 +57,14 @@ function loadTopicPage(event) {
 }
 
 function prepOfflineQuestions(event) {
-  /* 
-  position = Index, welche Aufgabe genommen werden soll
-  */
   // prevPage = "loadTopicPage(Offline)"; //Muss noch angepasst werden (event)
   clearMain();
   try {
     const targetElement = event.target || event.srcElement;
     topic = targetElement.name;
   } catch (error) {
+    console.log("Funktion wurde ohne Event ausgelöst.")
+    console.log(error)
   }
   loadOfflineQuestions(topic, 0);
 }
@@ -73,6 +75,7 @@ function loadOfflineQuestions(topic, position){
 
   if(position > data.length - 1){
     console.log("Kategorie abgeschlossen");
+    loadStats();
     return;
   }
 
@@ -80,6 +83,7 @@ function loadOfflineQuestions(topic, position){
   const quest = document.createElement("div");
   quest.setAttribute("id", "question");
   quest.setAttribute("system_identifier", position);
+  quest.setAttribute("name", data[position].a);
   quest.innerHTML = data[position].a;
   main.appendChild(quest);
 
@@ -88,26 +92,70 @@ function loadOfflineQuestions(topic, position){
   solution.setAttribute("class", "solution");
 
   const mixedSolutions = randomizeArray(data[position].l);
-  createButtonsFromArray(mixedSolutions, solution, submitAnswer);
+  createButtonsFromArray(mixedSolutions, solution, submitOfflineAnswer);
   main.appendChild(solution);
+
+  // console.log(Object.keys(statsOffline));
+  // console.log(Object.keys(statsOffline).indexOf(topic))
+  if(Object.keys(statsOffline).indexOf(topic) == -1){ //Key noch nicht vorhanden
+    // console.log("Key noch nicht vorhanden");
+    statsOffline[topic] = {"Richtig": 0, "Falsch":0};
+    // console.log(statsOffline);
+  }
 }
 
-function submitAnswer(event) {
+function loadStats(){
+  clearMain();
+  const statsDiv = document.createElement("div");
+  statsDiv.setAttribute("class", "stats");
+
+  const keys = Object.keys(statsOffline);
+  for(let i in keys){
+    let topic = document.createElement("p");
+    topic.setAttribute("class", "topic");
+    topic.innerHTML = keys[i];
+
+    const ul = document.createElement("ul");
+    const subKeys = Object.keys(statsOffline[keys[i]]);
+    for(let j in subKeys){
+      const li = document.createElement("li");
+      li.innerHTML = subKeys[j] + ": " + statsOffline[keys[i]][subKeys[j]];
+      // console.log(subKeys[j]);
+      // console.log(statsOffline[keys[i]][subKeys[j]]);
+      ul.appendChild(li);
+    }
+    statsDiv.appendChild(topic);
+    statsDiv.appendChild(ul);
+  }
+
+  main.appendChild(statsDiv);
+}
+
+// Prüfen, ob die gewählte Antwort richtig war
+function submitOfflineAnswer(event) {
   const targetElement = event.target || event.srcElement;
   const buttonName = targetElement.name;
-  const questSet = document.getElementById("question");
-  const questSetId = questSet.getAttribute("system_identifier");
-
-  // Anfrage, welchen Index das Element im quellArray besitzt
-  let index = sourceData[topic][questSetId]["l"].indexOf(buttonName);
+  const question = document.getElementById("question");
+  const questionName = question.getAttribute("name");
+  const questionId = question.getAttribute("system_identifier");
+  
+  // const sourceQuestion = sourceData[topic].findIndex(element => element["a"] = questionName);
+  const index = 0; //sourceData[topic][sourceQuestion]["l"].indexOf(buttonName);
+  
   if(index == 0){
-    userFeedback(true, parseInt(questSetId)+1, sourceData[topic].length);
+    // Bei richtiger Antwort
+    userFeedback(true, parseInt(questionId)+1, sourceData[topic].length);
+    statsOffline[topic]["Richtig"] = statsOffline[topic]["Richtig"] + 1;
+    // console.log(statsOffline);
   }else{
-    userFeedback(false, parseInt(questSetId)+1, sourceData[topic].length);
+    // bei falscher Antwort
+    userFeedback(false, parseInt(questionId)+1, sourceData[topic].length);
+    statsOffline[topic]["Richtig"] = statsOffline[topic]["Falsch"] + 1;
   }
-  setTimeout(loadOfflineQuestions,500,topic, parseInt(questSetId)+1);
+  setTimeout(loadOfflineQuestions,500,topic, parseInt(questionId)+1);
 }
 
+// Gibt ein visuelles Feedback, ob die Frage richtig oder falsch beantwortet wurde.
 function userFeedback(state, current, total){
   let color, text;
   if(state){
@@ -160,3 +208,5 @@ function createButtonsFromArray(input, where, listenerFunction) {
     where.appendChild(el);
   }
 }
+
+// Lösung des Array Problems, alle Aufgaben inkl. Lösungen in ein Stringarry davon dann die Reihenfolge tauschen und dann wie gewohnt weitermachen
