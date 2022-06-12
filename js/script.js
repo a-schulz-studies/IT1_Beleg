61ce2cfa0c53c60e05a3c1539ff350a716cb7b23
Alexander Schulz <s82105@htw-dresden.de>
2022-06-12 16:35:59 +0200

    Kommunikation mit dem Server funktioniert

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // TODO:
 /* 
+- afterSubmit nochmal prüfen
+- ShowStats als Button oben in der Leiste
+
 - Responsive Design mittels mediaquerys
+-PWA
+
+-Misserfolge:
+  Umstellung xhr -> fetch
+  zufällige Reihenfolge der Aufgaben -> identifikation der Elemente war zum Teil schwierig
+  Code wird schnell unübersichtlich
  */
 
 // Initializig Functionality
@@ -14,6 +23,7 @@ var prevPage = "";
 let topic;
 let stats = {};
 let questionIds;
+let serverData;
 
 /* Start
 startet die Funktionalität
@@ -28,7 +38,9 @@ function loadStartPage(div) {
   clearMain();
   adjustProgressbar("0%");
   topic = "";
-  prevPage = function(){loadStartPage(main)};
+  prevPage = function () {
+    loadStartPage(main);
+  };
   let options = ["Offline", "Online"];
   const inner = document.createElement("div");
   inner.setAttribute("id", "options");
@@ -38,7 +50,9 @@ function loadStartPage(div) {
 
 //TopicPage - Hier werden die verschiedenen Themen der gewählten Option angezeigt
 function loadTopicPage(buttonName) {
-  prevPage = function(){loadStartPage(main)};
+  prevPage = function () {
+    loadStartPage(main);
+  };
   clearMain();
   adjustProgressbar("0%");
   topic = "";
@@ -49,35 +63,142 @@ function loadTopicPage(buttonName) {
       createButtonsFromArray(topics, main, prepOfflineQuestions, topics);
       break;
     case "Online":
-      loadOnlineQuestions();
+      prepOnlineQuestions();
       break;
   }
 }
 
+// Funktion zur Kommunikation mit den Server
 function sendRequest(url, method, body = "") {
   const options = {
-      method: method,
-      headers: new Headers({
-        'content-type': 'application/json',
-        'Authorization': "Basic " + window.btoa(username + ":" + password)
-      }),
-      // mode: 'no-cors'
+    method: method,
+    headers: new Headers({
+      "content-type": "application/json",
+      Authorization: "Basic " + window.btoa(username + ":" + password),
+    }),
   };
 
-  if(method == "POST"){
-    options.body = JSON.stringify(body);
+  if (method == "POST") {
+    options.body = body;
   }
 
   return fetch(url, options);
 }
 
 // Läd die OnlineFragen
-function loadOnlineQuestions(){
-  sendRequest(url, "GET")
-  .then(response => response.json())
-  .then(data => console.log(data));
+function prepOnlineQuestions() {
+  const result = sendRequest(url, "GET")
+    .then((response) => response.json())
+    .then((data) => (serverData = data.content))
+    .then(
+      () =>
+        (questionIds = randomizeArray(
+          createNumberedArray(serverData.length)
+        ))
+    )
+    .then(loadOnlineQuestions);
 }
 
+function loadOnlineQuestions() {
+  // console.log(serverData);
+  topic = "Online";
+  prevPage = function () {
+    loadStartPage(main);
+  };
+  clearMain();
+
+  const position = questionIds.shift();
+
+  if (position == undefined) {
+    loadStats(false);
+    return;
+  }
+
+    // Titel erstellen
+    const title = document.createElement("div");
+    title.setAttribute("id", "title");
+    title.setAttribute("system_identifier", position);
+    title.setAttribute("name", serverData[position]["title"]);
+    title.innerHTML = "Titel: " + serverData[position]["title"];
+    main.appendChild(title);
+
+  // Aufgabenstellung erstellen
+  const quest = document.createElement("div");
+  quest.setAttribute("id", "question");
+  quest.setAttribute("system_identifier", position);
+  quest.setAttribute("name", serverData[position]["text"]);
+  quest.innerHTML = serverData[position]["text"];
+  main.appendChild(quest);
+
+  // Mögliche Lösungen erstellen
+  const solution = document.createElement("div");
+  solution.setAttribute("class", "solution");
+
+  const mixedSolutions = randomizeArray(serverData[position]["options"]);
+  createCheckboxFromArray(
+    mixedSolutions,
+    solution
+  );
+  const submit = document.createElement("button");
+  submit.setAttribute("name", "submit");
+  submit.innerHTML = "Weiter";
+  
+  submit.addEventListener("click", submitOnlineAnswer, false);
+  
+  main.appendChild(solution);
+  main.appendChild(submit);
+
+  if (Object.keys(stats).indexOf(topic) == -1) {
+    //Key noch nicht vorhanden -> neu erstellen
+    stats[topic] = { Richtig: 0, Falsch: 0 };
+  }
+}
+
+// Überprüfen der Angewählten Antworten - Online
+function submitOnlineAnswer(){
+  const question = document.getElementById("question").getAttribute("system_identifier");
+  
+  // Hier stehen die Namen der angewählten Boxen
+  const answer = getCheckedBoxes();
+  // console.log(answer);
+  let solutions = [];
+  for(let i in answer){
+    const index = serverData[question]["options"].indexOf(answer[i]);
+    solutions.push(index);
+  }
+  console.log(solutions);
+
+  if(solutions.length == 0){
+    solutions = "";
+  }else{
+    solutions = solutions.toString();
+  }
+  const id = parseInt(question) + 1;
+
+  // Anfrage beim Server, ob das Ergebnis richtig ist.
+  sendRequest(url + id + "/solve", "POST", "[" + solutions + "]")
+  .then(response => response.json())
+  .then(data => afterSubmit(data["success"]));
+}
+
+function afterSubmit(state){
+  if(state){
+    userFeedback(
+      true,
+      serverData.length - questionIds.length,
+      serverData.length
+    );
+    stats["Online"]["Richtig"] = stats[topic]["Richtig"] + 1;
+  }else{
+    userFeedback(
+      false,
+      serverData.length - questionIds.length,
+      serverData.length
+    );
+    stats["Online"]["Falsch"] = stats[topic]["Falsch"] + 1;
+  }
+  setTimeout(loadOnlineQuestions, 500);
+}
 
 // Helper Function für die loadOfflineQuestions
 function prepOfflineQuestions(buttonName) {
@@ -88,7 +209,9 @@ function prepOfflineQuestions(buttonName) {
 
 // Rendert die Fragestellungen und die Lösungsmöglichkeiten
 function loadOfflineQuestions(topic) {
-  prevPage = function(){loadTopicPage("Offline")};
+  prevPage = function () {
+    loadTopicPage("Offline");
+  };
   clearMain();
   const data = sourceData[topic];
   const position = questionIds.shift();
@@ -115,7 +238,12 @@ function loadOfflineQuestions(topic) {
   solution.setAttribute("class", "solution");
 
   const mixedSolutions = randomizeArray(data[position].l);
-  createButtonsFromArray(mixedSolutions, solution, submitOfflineAnswer, mixedSolutions);
+  createButtonsFromArray(
+    mixedSolutions,
+    solution,
+    submitOfflineAnswer,
+    mixedSolutions
+  );
   main.appendChild(solution);
 
   if (Object.keys(stats).indexOf(topic) == -1) {
@@ -133,7 +261,9 @@ function loadOfflineQuestions(topic) {
 // Zeigt die Statistikseite an, clearStats = true löscht die Statistik
 function loadStats(clearStats) {
   // Dadurch wird die Statistik gelöscht
-  if(clearStats){stats = {}}
+  if (clearStats) {
+    stats = {};
+  }
   clearMain();
   topic = "";
 
@@ -158,26 +288,45 @@ function loadStats(clearStats) {
   }
   main.appendChild(statsDiv);
 
-  const additionalOptions = ["Anderes Thema wählen", "Offline oder Online?", "Statistik löschen"];
+  const additionalOptions = [
+    "Anderes Thema wählen",
+    "Offline oder Online?",
+    "Statistik löschen",
+  ];
   const additionalEventListeners = [loadTopicPage, loadStartPage, loadStats];
   const additionalListenerParams = ["Offline", main, true];
 
-  createButtonsFromArray(additionalOptions, main, additionalEventListeners, additionalListenerParams);
+  createButtonsFromArray(
+    additionalOptions,
+    main,
+    additionalEventListeners,
+    additionalListenerParams
+  );
 }
 
 // Prüfen, ob die gewählte Antwort richtig war
 function submitOfflineAnswer(buttonName) {
   const question = document.getElementById("question").getAttribute("name");
-  const currentData = sourceData[topic].filter(element => element["a"] == question);
+  const currentData = sourceData[topic].filter(
+    (element) => element["a"] == question
+  );
 
   const index = currentData[0]["l"].indexOf(buttonName);
   if (index == 0) {
     // Bei richtiger Antwort
-    userFeedback(true, sourceData[topic].length - questionIds.length, sourceData[topic].length);
+    userFeedback(
+      true,
+      sourceData[topic].length - questionIds.length,
+      sourceData[topic].length
+    );
     stats[topic]["Richtig"] = stats[topic]["Richtig"] + 1;
   } else {
     // bei falscher Antwort
-    userFeedback(false, sourceData[topic].length - questionIds.length, sourceData[topic].length);
+    userFeedback(
+      false,
+      sourceData[topic].length - questionIds.length,
+      sourceData[topic].length
+    );
     stats[topic]["Falsch"] = stats[topic]["Falsch"] + 1;
   }
   setTimeout(loadOfflineQuestions, 500, topic);
@@ -250,10 +399,12 @@ function createButtonsFromArray(
           },
           false
         );
-      }else { //Mehrere Funktionen + jeweiliger Parameter ist nicht vorhanden
+      } else {
+        //Mehrere Funktionen + jeweiliger Parameter ist nicht vorhanden
         el.addEventListener("click", listenerFunction[i], false);
       }
-    } else { // Nur eine Funktion
+    } else {
+      // Nur eine Funktion
       // Nur eine Funktion + jeweiliger Parameter ist vorhanden
       if (functionParam != [] && functionParam[i] != "") {
         el.addEventListener(
@@ -280,15 +431,43 @@ function adjustProgressbar(percent) {
 }
 
 // Läd die vorherige Seite
-function loadPrevPage(){
+function loadPrevPage() {
   prevPage();
 }
 
 // Erzeugt ein durchnummeriertes Array mit der vorgegebenen Länge
-function createNumberedArray(length){
-      let array = [];
-      for(let i = 0; i < length; i++){
-        array[i] = i;
-      }
-      return array;
+function createNumberedArray(length) {
+  let array = [];
+  for (let i = 0; i < length; i++) {
+    array[i] = i;
+  }
+  return array;
+}
+
+// Erstellt Radiobuttons und zeigt sie in entsprechendem Element an
+function createCheckboxFromArray(input, where){
+  for(let i in input){
+    const label = document.createElement("label");
+    label.innerHTML = input[i];
+    const el = document.createElement("input");
+    el.setAttribute("type", "checkbox");
+    el.setAttribute("name", input[i]);
+    el.setAttribute("system_identifier", i);
+    label.appendChild(el);
+    where.appendChild(label);
+  }
+}
+
+function getCheckedBoxes() {
+  var checkboxes = document.querySelectorAll('input[type=checkbox]')
+  var checkboxesChecked = [];
+  // loop over them all
+  for (var i=0; i<checkboxes.length; i++) {
+     // And stick the checked ones onto an array...
+     if (checkboxes[i].checked) {
+        checkboxesChecked.push(checkboxes[i].getAttribute("name"));
+     }
+  }
+  // Return the array if it is non-empty, or null
+  return checkboxesChecked.length > 0 ? checkboxesChecked : null;
 }
--- notes.js
+++ notes.js
@@ -1,163 +1,119 @@
-p = JSON.parse(text);
-    console.log(p.note);
-    console.log(p.note[1]);
-    console.log(p.note[1].a);
-    document.getElementById("ziel").innerHTML = text;
+<div class="auswahl">
+                  <h3>BLOCKAUSWAHL</h3>
+                  <form>
+                     <fieldset>
+                  <input class="choices" type="radio" id="A" name="Auswahl"/>
+                  <label for="A">A</label> 
 
-    (async () => {
-        let response = await new Promise(resolve => {
-           var xhr = new XMLHttpRequest();
-           xhr.open("GET", url, true);
-           xhr.onload = function(e) {
-             resolve(xhr.response);
-           };
-           xhr.onerror = function () {
-             resolve(undefined);
-             console.error("** An error occurred during the XMLHttpRequest");
-           };
-           xhr.send();
-        }) 
-        doTheThing(response)
-     })()
-// für versenden des Passwords im klartext
-     xhr.setRequestHeader("Authorization", "Basic " + window.btoa("name:pw"));
-
+[
     {
-        "content": [
-            {
-                "id": 1,
-                "title": "The Java Logo",
-                "text": "What is depicted on the Java logo?",
-                "options": [
-                    "Robot",
-                    "Tea leaf",
-                    "Cup of coffee",
-                    "Bug"
-                ]
-            },
-            {
-                "id": 2,
-                "title": "REST",
-                "text": "What is REST",
-                "options": [
-                    "a",
-                    "b",
-                    "c",
-                    "d"
-                ]
-            },
-            {
-                "id": 3,
-                "title": "The Java Logo",
-                "text": "What is depicted on the Java logo?",
-                "options": [
-                    "Robot",
-                    "Tea leaf",
-                    "Cup of coffee",
-                    "Bug"
-                ]
-            },
-            {
-                "id": 4,
-                "title": "Bananarama",
-                "text": "Which form of measurement is the best?",
-                "options": [
-                    "Metric",
-                    "Imperial",
-                    "Banana for Scale",
-                    "all of the above"
-                ]
-            },
-            {
-                "id": 5,
-                "title": "The Java Logo",
-                "text": "What is depicted on the Java logo?",
-                "options": [
-                    "Robot",
-                    "Tea leaf",
-                    "Cup of coffee",
-                    "Bug"
-                ]
-            },
-            {
-                "id": 9,
-                "title": "Aladdin",
-                "text": "In welcher Stadt lebt Aladdin?",
-                "options": [
-                    "Agrabah",
-                    "Baserdi",
-                    "Djelly",
-                    "Costella"
-                ]
-            },
-            {
-                "id": 10,
-                "title": "Simpsons",
-                "text": "Was ist der Vorname von Chief Wiggum",
-                "options": [
-                    "Boris",
-                    "Clancy",
-                    "Arthur",
-                    "David"
-                ]
-            },
-            {
-                "id": 11,
-                "title": "Universum",
-                "text": "Was ist die Antwort auf die Frage nach dem Leben, dem Universum und Allem?",
-                "options": [
-                    "2000",
-                    "73",
-                    "42",
-                    "8"
-                ]
-            },
-            {
-                "id": 12,
-                "title": "The Java Logo",
-                "text": "What is depicted on the Java logo?",
-                "options": [
-                    "Robot",
-                    "Tea leaf",
-                    "Cup of coffee",
-                    "Bug"
-                ]
-            },
-            {
-                "id": 13,
-                "title": "The Java Logo",
-                "text": "What is depicted on the Java logo?",
-                "options": [
-                    "Robot",
-                    "Tea leaf",
-                    "Cup of coffee",
-                    "Bug"
-                ]
-            }
-        ],
-        "pageable": {
-            "sort": {
-                "sorted": false,
-                "unsorted": true,
-                "empty": true
-            },
-            "offset": 0,
-            "pageNumber": 0,
-            "pageSize": 10,
-            "unpaged": false,
-            "paged": true
-        },
-        "last": false,
-        "totalPages": 5,
-        "totalElements": 42,
-        "number": 0,
-        "sort": {
-            "sorted": false,
-            "unsorted": true,
-            "empty": true
-        },
-        "size": 10,
-        "first": true,
-        "numberOfElements": 10,
-        "empty": false
+        "id": 1,
+        "title": "The Java Logo",
+        "text": "What is depicted on the Java logo?",
+        "options": [
+            "Robot",
+            "Tea leaf",
+            "Cup of coffee",
+            "Bug"
+        ]
+    },
+    {
+        "id": 2,
+        "title": "REST",
+        "text": "What is REST",
+        "options": [
+            "a",
+            "b",
+            "c",
+            "d"
+        ]
+    },
+    {
+        "id": 3,
+        "title": "The Java Logo",
+        "text": "What is depicted on the Java logo?",
+        "options": [
+            "Robot",
+            "Tea leaf",
+            "Cup of coffee",
+            "Bug"
+        ]
+    },
+    {
+        "id": 4,
+        "title": "Bananarama",
+        "text": "Which form of measurement is the best?",
+        "options": [
+            "Metric",
+            "Imperial",
+            "Banana for Scale",
+            "all of the above"
+        ]
+    },
+    {
+        "id": 5,
+        "title": "The Java Logo",
+        "text": "What is depicted on the Java logo?",
+        "options": [
+            "Robot",
+            "Tea leaf",
+            "Cup of coffee",
+            "Bug"
+        ]
+    },
+    {
+        "id": 9,
+        "title": "Aladdin",
+        "text": "In welcher Stadt lebt Aladdin?",
+        "options": [
+            "Agrabah",
+            "Baserdi",
+            "Djelly",
+            "Costella"
+        ]
+    },
+    {
+        "id": 10,
+        "title": "Simpsons",
+        "text": "Was ist der Vorname von Chief Wiggum",
+        "options": [
+            "Boris",
+            "Clancy",
+            "Arthur",
+            "David"
+        ]
+    },
+    {
+        "id": 11,
+        "title": "Universum",
+        "text": "Was ist die Antwort auf die Frage nach dem Leben, dem Universum und Allem?",
+        "options": [
+            "2000",
+            "73",
+            "42",
+            "8"
+        ]
+    },
+    {
+        "id": 12,
+        "title": "The Java Logo",
+        "text": "What is depicted on the Java logo?",
+        "options": [
+            "Robot",
+            "Tea leaf",
+            "Cup of coffee",
+            "Bug"
+        ]
+    },
+    {
+        "id": 13,
+        "title": "The Java Logo",
+        "text": "What is depicted on the Java logo?",
+        "options": [
+            "Robot",
+            "Tea leaf",
+            "Cup of coffee",
+            "Bug"
+        ]
     }
+]
